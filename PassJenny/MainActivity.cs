25635aa8a30f7704ac0dab8f5692d897beb0c6ea
tlevans3543 <tevans96@comcast.net>
2020-12-09 10:28:24 -0500

    Password generator app for Android v1.0 - activity_main.xml -- Added layout elements and context
    - MainActivity.cs
    -- Mapped layout elements to data variables
    -- Call generator method and display to TextView
    
    - PasswordGenerator.cs
    -- new file
    -- Generates a password based on user selection of valid chars

--- PassJenny/MainActivity.cs
+++ PassJenny/MainActivity.cs
@@ -13,9 +13,34 @@ namespace PassJenny
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            // Wire up
+
+            // UI controls
+            CheckBox uppercase = FindViewById<CheckBox>(Resource.Id.checkBoxUpper);
+            CheckBox numbers = FindViewById<CheckBox>(Resource.Id.checkBoxNumbers);
+            CheckBox specialchars = FindViewById<CheckBox>(Resource.Id.checkBoxSpecial);
+            EditText passLength = FindViewById<EditText>(Resource.Id.passLength);
+            Button generatePass = FindViewById<Button>(Resource.Id.generateButton);
+            TextView generatedPassword = FindViewById<TextView>(Resource.Id.generatedPassword);
+
+            // Code to generate password
+            generatePass.Click += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(passLength.Text))
+                {
+                    generatedPassword.Text = string.Empty;
+                }
+                else
+                {
+                    generatedPassword.Text = Core.PasswordGenerator.GeneratePassword(passLength.Text, uppercase.Checked, numbers.Checked, specialchars.Checked);
+                }
+            };
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
---
+++ PassJenny/PasswordGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System;
+
+namespace Core
+{
+    public class PasswordGenerator
+    {
+        public static string GeneratePassword(string passLength, bool upperBool, bool numBool, bool specBool)
+        {
+
+            string validChars = "abcdefghijklmnopqrstuvwxyz";
+
+            string upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string numChars = "0123456789";
+            string specialChars = "@!#$%^&*";
+
+            StringBuilder generatedPassword = new StringBuilder();
+            int length = Int32.Parse(passLength);
+
+            if (upperBool == true)
+            {
+                validChars += upperChars;
+            }
+            if (numBool == true)
+            {
+                validChars += numChars;
+            }
+            if (specBool == true)
+            {
+                validChars += specialChars;
+            }
+
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                generatedPassword.Append(validChars[rnd.Next(validChars.Length)]);
+            }
+
+            return generatedPassword.ToString();
+        }
+    }
+}
